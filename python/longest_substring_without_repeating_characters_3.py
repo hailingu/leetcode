08a23ddae04e2793a308c8691f6c1232ded4f43f
Gu Hailin <linhaigu@gmail.com>
2019-10-03 15:32:06 +0800

    update solution to question 3

--- python/longest_substring_without_repeating_characters_3.py
+++ python/longest_substring_without_repeating_characters_3.py
@@ -8,19 +8,14 @@ class Solution(object):
         d = {}
         o = 0
         max_len = 0
-        tmp_len = 0
         i = 0
 
         for c in s:
-            if c not in d or (c in d and o > d[c]):
-                d[c] = i
-            else:
-                tmp_len = i - o
-                max_len = tmp_len if tmp_len > max_len else max_len
-                o = d[c] + 1
-                d[c] = i
+            if c in d:
+                o = max(d[c], o)
+            
+            max_len = max(max_len, i - o + 1)
+            d[c] = i + 1
             i += 1
 
-        tmp_len = i - o
-        max_len = tmp_len if tmp_len > max_len else max_len
         return max_len
