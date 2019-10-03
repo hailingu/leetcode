5af6e2e6af73f5378bac34916117d972593c07ef
hailingu <linhaigu@gmail.com>
2019-10-03 13:16:16 +0800

    added solution to question 1

---
+++ python/two_sum_1.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        idx = 0
+        d = {}
+        
+        for num in nums:
+            t = target - num
+            if d.get(t) is not None:
+                return [d.get(t), idx]
+            else:
+                d[num] = idx
+            idx = idx + 1
+        
