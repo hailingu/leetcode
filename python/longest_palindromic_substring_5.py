305bcad0e677445bb67a1c916de2825fff1de0fb
hailingu <linhaigu@gmail.com>
2019-10-04 16:42:10 +0800

    add solution to question 5

---
+++ python/longest_palindromic_substring_5.py
@@ -0,0 +1,43 @@
+class Solution(object):
+    def longestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        if s == None or len(s) == 0:
+            return s
+        
+        nStr = '#'
+        for c in s:
+            nStr = nStr + c + '#'
+        
+        p = [0] * len(nStr)
+        p[0] = 1
+        idt = 0
+        mx = 0
+        hId = 0
+        hmx = 0
+
+        i = 1
+        while i < len(nStr):
+            p[i] = 1 if i < mx else min(p[2 * idt -i], mx - i)
+            while i + p[i] < len(nStr) and i >= p[i] and nStr[i + p[i]] == nStr[i - p[i]]:
+                p[i] += 1
+
+            if i + p[i] > mx:
+                mx = i + p[i]
+                idt = i
+
+                if p[i] > p[hId]:
+                    hId = idt
+                    hmx = mx
+            i += 1
+
+        ans = ''
+        r = hmx - hId - 1 
+        tmp = nStr[hId - r: hId + r]
+        for c in tmp:
+            if c != '#':
+                ans += c
+
+        return ans
